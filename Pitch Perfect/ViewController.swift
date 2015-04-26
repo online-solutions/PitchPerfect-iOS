cdf20295114c53fd81eafafdf51cf6c4d2481b15
Yohanan <yohananjr13@gmail.com>
2015-04-26 17:08:31 +0700

    Initial

---
+++ Pitch Perfect/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Pitch Perfect
+//
+//  Created by KhanhPhung on 25/4/15.
+//  Copyright (c) 2015 Online Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var recordInProcess: UILabel!
+    @IBOutlet weak var stopRecordAudio: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func recordAudio(sender: UIButton) {
+        recordInProcess.hidden = false
+        stopRecordAudio.hidden = false
+        //TODO: record user's voice
+        println("in recordAudio func")
+    }
+    
+    @IBAction func saveAudio(sender: UIButton) {
+        recordInProcess.hidden = true
+        // TODO: change stop icon to another icon
+    }
+    
+
+}
+
